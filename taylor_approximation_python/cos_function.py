8eb5a1c165a589e128502268561fb6ac71e46ac5
Md Jibanul Haque Jiban <jibanul@knights.ucf.edu>
2020-10-30 23:03:51 -0400

    Update cos_function.py

--- taylor_approximation_python/cos_function.py
+++ taylor_approximation_python/cos_function.py
@@ -4,25 +4,19 @@ from IPython import display
 import numpy as np
 
 def taylor_approx(n):
-    x = np.linspace(np.pi*(-3), np.pi*(3),100)
+    x = np.linspace(np.pi*(-3), np.pi*(3),100)                          # values of x from -3*pi to 3*pi 
     approx = np.zeros(len(x), dtype=np.dtype('O'))
     for i in range(n):
-        
-        # print("results", ((-1)**i * (x**(2*i)/np.math.factorial(2*i))).shape)
-        # print(np.dtype(((-1)**i) * (x**(2*i)/np.math.factorial(2*i))))
-        approx += ((-1)**i) * (x**(2*i)/np.math.factorial(2*i))
+        approx += ((-1)**i) * (x**(2*i)/np.math.factorial(2*i))         # taylor series of cos(x)
         pl.plot(x, np.cos(x))
         pl.plot(x, approx)
         pl.ylim(-1.7,1.7)
-        # pl.xlim(-12,12)
         pl.legend(('cos() function', 'Taylor Approximation'), loc = 'upper right')
         plt.text(x = -8, y = 1.25, s = "n = " + str(i), fontsize=15)
-        # pl.xlim(-1,1)
         display.clear_output(wait=True)
         display.display(pl.gcf())
-        
         plt.clf()
-        time.sleep(1.0)        
-        # print(i)
+        time.sleep(1.0)        # pause 1 sec before ploting the next curve
 
-taylor_approx(15)
+# call the function
+taylor_approx(n = 13)
